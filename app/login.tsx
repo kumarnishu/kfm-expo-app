2955f90b1f2346e11362e9a7a8e9b0ec8eeb3f5d
nishu kumar <kumarnishu437@gmail.com>
2024-11-24 13:06:54 +0530

    commit

--- app/login.tsx
+++ app/login.tsx
@@ -1,16 +1,141 @@
 import React from 'react'
 import { ThemedView } from '@/components/elements/ThemedView'
-import { ThemedText } from '@/components/elements/ThemedText'
-import { Button } from 'react-native'
-import {  useRouter } from 'expo-router'
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../contexts/UserContext';
+import { Login } from '../services/UserServices';
+import {
+    Alert,
+    Button,
+    ScrollView,
+    StyleSheet,
+    TextInput,
+    View
+} from 'react-native';
+import { useMutation } from 'react-query';
+import { AxiosResponse } from 'axios';
+import { BackendError } from '..';
+import { router } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { GetUserDto } from '@/dto';
 
 const login = () => {
-    const router=useRouter()
+    const { setUser } = useContext(UserContext);
+    const [username, setUsername] = useState<string | null>(null);
+    const [password, setPassword] = useState<string | null>(null);
+
+    const { mutate, data, isSuccess, isLoading, error } = useMutation<
+        AxiosResponse<{ user: GetUserDto; token: string }>,
+        BackendError,
+        { username: string; password: string; multi_login_token?: string }
+    >(Login);
+
+    useEffect(() => {
+        const retrieveCredentials = async () => {
+            const savedUsername = await AsyncStorage.getItem('uname');
+            const savedPassword = await AsyncStorage.getItem('passwd');
+            setUsername(savedUsername || '');
+            setPassword(savedPassword || '');
+        };
+        retrieveCredentials();
+    }, []);
+
+    useEffect(() => {
+        if (isSuccess && data) {
+            setUser(data.data.user);
+            router.replace('/');
+            AsyncStorage.setItem('uname', username || '');
+            AsyncStorage.setItem('passwd', password || '');
+        }
+        if (error) {
+            Alert.alert(error?.response?.data?.message || 'Check internet connection !!');
+        }
+    }, [isSuccess, error]);
+
+    const handleSubmit = () => {
+        if (username && password) {
+            mutate({ username, password });
+        } else {
+            Alert.alert('Please enter both username and password.');
+        }
+    };
+
     return (
-        <ThemedView>
-            <ThemedText>Login page</ThemedText>
-        </ThemedView>
-    )
-}
+        <ScrollView>
+            <View style={styles.form}>
+                <TextInput
+                    placeholder="Username, Email, or Mobile"
+                    value={username || ''}
+                    onChangeText={setUsername}
+                    autoCapitalize="none"
+                />
+
+                <TextInput
+                    placeholder="Enter your password"
+                    value={password || ''}
+                    onChangeText={setPassword}
+                    secureTextEntry
+                    autoCapitalize="none"
+                />
+
+                <Button
+                    title="login"
+                    onPress={handleSubmit}
+                    disabled={isLoading}
+                />
+            </View>
+        </ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    form: {
+        flex: 1,
+        borderWidth: 2,
+        borderColor: 'whitesmoke',         // Light red border color
+        borderRadius: 30,
+        marginTop: 200,
+        margin: 10,
+        padding: 25,
+        paddingVertical: 50,
+        backgroundColor: '#f9f9f9',     // Light background color for better visibility
+
+        // Shadow properties for iOS
+        shadowColor: '#000',            // Shadow color
+        shadowOffset: { width: 0, height: 2 }, // Offset of the shadow
+        shadowOpacity: 0.3,             // Shadow opacity
+        shadowRadius: 2,                // Shadow blur radius
+
+        // Elevation for Android
+        elevation: 1,                   // Controls the shadow for Android
+    },
+
+
+    logo: {
+        height: 130,
+        width: 130,
+        borderRadius: 85,              // Makes the logo circular
+        borderWidth: 2,
+        marginBottom: 25,
+        shadowColor: '#000',           // Shadow color
+        shadowOffset: { width: 0, height: 2 }, // Offset of the shadow
+        shadowOpacity: 0.3,            // Shadow opacity
+        shadowRadius: 5,               // Shadow blur radius
+        elevation: 5,                   // Elevation for Android
+        alignSelf: 'center',            // Center the logo horizontally
+    },
+    button: {
+        backgroundColor: '#FF4D4D',
+        borderRadius: 15,
+        marginHorizontal: 5,
+        marginVertical: 5,
+        minHeight: 50
+    },
+    buttonText: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        textTransform: 'capitalize',
+        letterSpacing: 1.5
+    },
+});
 
 export default login
